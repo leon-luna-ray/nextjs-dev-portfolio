f7c059e00baf782400c6c041165ff2b2f20f4e65
Ray Luna <leon.luna.ray@gmail.com>
2021-06-12 14:01:52 -0700

    import boilerplate

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,56 +3,12 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import {
-  Container,
-  Div1,
-  Div2,
-  Div3,
-  NavLink,
-  SocialIcons,
-} from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
-const Header = () => (
-  <Container>
-    <Div1>
-      <Link href='/'>
-        <a style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
-          <DiCssdeck size='3rem' /> <span>Ray Luna</span>
-        </a>
-      </Link>
-    </Div1>
-    <Div2>
-      <li>
-        <Link href='#about'>
-          <NavLink>About</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href='#tech'>
-          <NavLink>Technologies</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href='#projects'>
-          <NavLink>Projects</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href='#contact'>
-          <NavLink>Contact</NavLink>
-        </Link>
-      </li>
-    </Div2>
-    <Div3>
-      {/* add e-mail and resume links/icons here next */}
-      <SocialIcons href='https://google.com'>
-        <AiFillGithub size='3rem' />
-      </SocialIcons>
-      <SocialIcons href='https://google.com'>
-        <AiFillLinkedin size='3rem' />
-      </SocialIcons>
-    </Div3>
-  </Container>
+const Header = () =>  (
+  <div>
+    Header
+  </div>
 );
 
 export default Header;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,30 +1,13 @@
 import React from 'react';
 
-import {
-  Section,
-  SectionText,
-  SectionTitle,
-} from '../../styles/GlobalComponents';
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = props => (
-  <>
-    <Section row nopadding>
-      <LeftSection>
-        <SectionTitle main center>
-          Ray Luna <br />
-          Developer Portfolio
-        </SectionTitle>
-        <SectionText>
-          I am a full-stack web developer using my skills to create useful
-          human-centric web applications with an emphasis on ease of use and
-          simple design.
-        </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
-      </LeftSection>
-    </Section>
-  </>
+const Hero = (props) => (
+  <div>
+    Hero
+  </div>
 );
 
 export default Hero;
