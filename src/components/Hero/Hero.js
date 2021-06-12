2da2721532b62f7a79cc578c04217a3fc144103a
Ray Luna <leon.luna.ray@gmail.com>
2021-06-11 19:52:27 -0700

    import starter

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,10 +9,11 @@ const Hero = (props) => (
     <Section row nopadding>
       <LeftSection>
         <SectionTitle main center>
-          Portfolio
+          Welcome To <br />
+          My Personal Portfolio
         </SectionTitle>
         <SectionText>
-        Section Text
+        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
         </SectionText>
         <Button onClick={props.handleClick}>Learn More</Button>
       </LeftSection>
