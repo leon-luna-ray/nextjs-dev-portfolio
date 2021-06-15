b915cd4d247256b409eb9adde3ac1753ffb69bca
Ray Luna <leon.luna.ray@gmail.com>
2021-06-14 23:23:07 -0700

    header and hero complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { FaConnectdevelop } from 'react-icons/fa';
+import { AiFillGithub, AiFillLinkedin, AiTwotoneMail } from 'react-icons/ai';
+import { TiCode } from 'react-icons/ti';
 
 import {
   Container,
@@ -10,6 +10,7 @@ import {
   Div3,
   NavLink,
   SocialIcons,
+  Span,
 } from './HeaderStyles';
 
 const Header = () => (
@@ -17,10 +18,46 @@ const Header = () => (
     <Div1>
       <Link href='/'>
         <a style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
-          <FaConnectdevelop />
+          <TiCode size='4rem' /> <Span> Ray Luna</Span>
         </a>
       </Link>
     </Div1>
+    <Div2>
+      <li>
+        <Link href='#projects'>
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#technologies'>
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#about'>
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#contact'>
+          <NavLink>Contact</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href='https://github.com/leon-luna-ray' target='_blank'>
+        <AiFillGithub size='3rem' />
+      </SocialIcons>
+      <SocialIcons
+        href='https://www.linkedin.com/in/leon-luna-ray/'
+        target='_blank'
+      >
+        <AiFillLinkedin size='3rem' />
+      </SocialIcons>
+      <SocialIcons href='mailto:leon.luna.ray@gmail.com'>
+        <AiTwotoneMail size='3rem' />
+      </SocialIcons>
+    </Div3>
   </Container>
 );
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = props => (
+  <Section row nopading>
+    <LeftSection>
+      <SectionTitle main center>
+        Ray Luna <br />
+        Developer Portfolio
+      </SectionTitle>
+      <SectionText>
+        I am a full-stack web developer creating useful human-centric web
+        applications with an emphasis on ease of use and simple design.
+      </SectionText>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
