fbf2139ece1f6a4d3c2e77c3d16d45db9c1090a4
Ray Luna <leon.luna.ray@gmail.com>
2021-06-14 22:12:45 -0700

    import icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,27 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import { FaConnectdevelop } from 'react-icons/fa';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+} from './HeaderStyles';
 
-const Header = () =>  (
-  <div>
-    Header
-  </div>
+const Header = () => (
+  <Container>
+    <Div1>
+      <Link href='/'>
+        <a style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
+          <FaConnectdevelop />
+        </a>
+      </Link>
+    </Div1>
+  </Container>
 );
 
 export default Header;
