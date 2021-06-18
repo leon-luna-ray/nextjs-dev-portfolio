d4985ad22af79d6ee17c77918c5cbb664bd24c2a
Ray Luna <leon.luna.ray@gmail.com>
2021-06-17 22:03:48 -0700

    technologies section setup

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id='technologies'>
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I have worked with range of full-stack web development technologies
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiFirebase size='3rem' />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph></ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size='3rem' />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph></ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size='3rem' />
+        <ListContainer>
+          <ListTitle>Tools</ListTitle>
+          <ListParagraph></ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
