6c8830e7c79eb328f6249b89062f8e63a9850801
Ray Luna <leon.luna.ray@gmail.com>
2021-06-23 16:53:16 -0700

    update contact secion

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,9 +7,24 @@ import {
 } from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './ContactStyles';
 
+const data = [
+  { title: 'Github', contact: 'https://github.com/leon-luna-ray' },
+  { title: 'Linkedin', contact: 'https://www.linkedin.com/in/leon-luna-ray/' },
+  { title: 'Email', contact: 'mailto:leon.luna.ray@gmail.com' },
+  { title: 'Resume', contact: 'Download' },
+];
+
 const Contact = () => (
   <Section id='contact'>
     <SectionTitle>Contact</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxText>{card.title}</BoxText>
+          <BoxText>{card.contact}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
   </Section>
 );
 
