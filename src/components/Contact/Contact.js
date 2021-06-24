97f8a7a1705fba9c58952ad4013b3af6cf02b217
Ray Luna <leon.luna.ray@gmail.com>
2021-06-23 18:28:50 -0700

    change contact section styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,7 +10,7 @@ import { Box, Boxes, BoxNum, BoxText } from './ContactStyles';
 const data = [
   { title: 'Github', contact: 'https://github.com/leon-luna-ray' },
   { title: 'Linkedin', contact: 'https://www.linkedin.com/in/leon-luna-ray/' },
-  { title: 'Email', contact: 'mailto:leon.luna.ray@gmail.com' },
+  { title: 'Email', contact: 'leon.luna.ray@gmail.com' },
   { title: 'Resume', contact: 'Download' },
 ];
 
@@ -18,12 +18,18 @@ const Contact = () => (
   <Section id='contact'>
     <SectionTitle>Contact</SectionTitle>
     <Boxes>
-      {data.map((card, index) => (
-        <Box key={index}>
-          <BoxText>{card.title}</BoxText>
-          <BoxText>{card.contact}</BoxText>
-        </Box>
-      ))}
+      <Box>
+        <BoxText>Github</BoxText>
+      </Box>
+      <Box>
+        <BoxText>Linkedin</BoxText>
+      </Box>
+      <Box>
+        <BoxText>Email</BoxText>
+      </Box>
+      <Box>
+        <BoxText>Resume</BoxText>
+      </Box>
     </Boxes>
   </Section>
 );
