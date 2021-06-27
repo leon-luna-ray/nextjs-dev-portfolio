9fb2f60c44ae1534e90c2048f08da47470dfccdc
Ray Luna <leon.luna.ray@gmail.com>
2021-06-26 20:53:34 -0700

    spellcheck

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Contact from '../components/Contact/Contact';
-import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
+import BgAnimation from '../components/BackgroundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
