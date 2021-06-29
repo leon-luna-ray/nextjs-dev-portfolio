a58cc6d1a05eed342d464bd23d1d6900735a95c3
Ray Luna <leon.luna.ray@gmail.com>
2021-06-29 16:46:18 -0700

    update tech section styling

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,7 +4,7 @@ export const projects = [
     description:
       'This is a messenger app for keeping in touch with friends, family and new people! The app allows the user to create an account, search users by username and add new contacts. The public chat allows each user to communicate with other members of the Social Butterfly Community. This app is built on a serverless architecture using React and Firebase Live Database.',
     image: '/images/social-butterfly.png',
-    tags: ['React', 'JavaScript', 'Firebase', 'Bootstrap', 'Heroku'],
+    tags: ['React.js', 'Firebase', 'Bootstrap', 'Heroku'],
     link: 'https://messenger-app-rsl.herokuapp.com',
     github: 'https://github.com/messenger-app-team/messenger-app',
     id: 0,
@@ -14,7 +14,7 @@ export const projects = [
     description: `An online recipe book app that allows the user to view and save their favorite recipes. It is a Heroku app built using Node.js, Express.js, Handlebars, Sequelize, Passport and Materialize. The application allows the user to create an account with a email and password, add their personal recipes, view saved recipes, search for saved recipes and print the recipe in a book page format.
     `,
     image: '/images/recipe-book.png',
-    tags: ['Node.js', 'MySQL', 'Handlebars', 'Materialize', 'Eslint'],
+    tags: ['Node.js', 'MySQL', 'Handlebars', 'Materialize'],
     link: 'https://arcane-peak-55315.herokuapp.com/',
     github: 'https://github.com/kurtp23/recipe-book',
     id: 1,
@@ -24,7 +24,7 @@ export const projects = [
     description:
       'A whimsical web application which allows a user create their own MadLibz story by entering a set of words or generated randomly generated words based on part of speech! This app was built with jQuery and JavaScript incorporating two third-party word APIs.',
     image: '/images/madlibz.png',
-    tags: ['JavaScript', 'jQuery', 'HTML', 'CSS', 'Bulma'],
+    tags: ['JavaScript', 'jQuery', 'HTML', 'CSS'],
     link: 'https://teammadlibz.github.io/MadLibz/',
     github: 'https://github.com/TeamMadLibz/MadLibz/',
     id: 2,
@@ -34,7 +34,7 @@ export const projects = [
     description:
       'A fun burger ordering app! The user can order any burger they like. The burger will be saved in their burger queue and moved to the devoured side when the user clicks eat-da-burger! This is a Heroku app built with Node.js and Handlebars.',
     image: '/images/eatdaburger.png',
-    tags: ['Handlebars', 'MySQL', 'Node.Js', 'Express.js', 'Heroku'],
+    tags: ['Handlebars', 'MySQL', 'Node.Js', 'Express.js'],
     link: 'https://burger-app-rluna.herokuapp.com/',
     github: 'https://github.com/leon-luna-ray/burgers-app',
     id: 3,
@@ -44,7 +44,7 @@ export const projects = [
     description:
       'A command line application for busy bosses to quickly render a styled team HTML page for quick access! The app will prompt the end-user for their contact information as well as their team data. Once complete the app will generate a team.html file in the output folder containing everyone on the team. The app is built with Node.js with a focus on testing using Jest.',
     image: '/images/team-profile.png',
-    tags: ['Node.js', 'Bulma', 'HTML', 'Inquirer', 'Jest'],
+    tags: ['Node.js', 'BulmaCSS', 'Inquirer', 'Jest'],
     link: 'https://www.youtube.com/watch?v=_9L0reNTwvs',
     github: 'https://github.com/leon-luna-ray/team-profile-generator',
     id: 4,
@@ -54,7 +54,7 @@ export const projects = [
     description:
       'A progressive web app for tracking your budget while travelling and on the go. This app allows the user to enter and track expenses both online and offline. Offline storage is handled using a service worker file and manifest. The app will automatically update the NoSQL database as soon as the connection is restored. Built with a focus on testing using Jest.',
     image: '/images/budget-tracker.png',
-    tags: ['Node.js', 'Express.js', 'Jest', 'MongoDB', 'Mongoose'],
+    tags: ['Node.js', 'Express.js', 'Jest', 'MongoDB'],
     link: 'https://budget-tracker-rluna.herokuapp.com/',
     github: 'https://github.com/leon-luna-ray/budget-tracker/',
     id: 5,
