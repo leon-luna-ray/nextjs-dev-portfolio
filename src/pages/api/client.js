c484c2d0409d8312e9b04721616a53c3967c919a
Ray Luna <leon.luna.ray@gmail.com>
2022-06-03 22:42:55 -0700

    fix env setup for conetentful. client api call successful

--- src/pages/api/client.js
+++ src/pages/api/client.js
@@ -1,7 +1,7 @@
-import * as contentful from 'contentful';
+import { createClient } from 'contentful';
 
-export const client = contentful.createClient({
-    // Left off here need to figure out access token not working from env
-    space:process.env.REACT_APP_CONTETNFUL_SPACE_ID,
-    accessToken: process.env.REACT_APP_CONTENTFUL_API_ACCESS_TOKEN,
-})
+export const client = createClient({
+  // Left off here need to figure out access token not working from env
+  accessToken: process.env.NEXT_PUBLIC_CONTETNFUL_SPACE_ID,
+  space: process.env.NEXT_PUBLIC_CONTETNFUL_SPACE_ID,
+});
